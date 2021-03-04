d3ff3536e6cc915945a7e8e9a49131d9fcc9af03
Albert Kottke <albert.kottke@gmail.com>
2021-03-03 16:47:15 -0800

    Changed to rst.

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@
 
 from setuptools import setup, find_packages
 
-with open("README.md") as fp:
+with open("README.rst") as fp:
     readme = fp.read()
 
+with open("HISTORY.rst") as fp:
+    history = fp.read()
+
 setup(
-    name="pygeems",
+    name="pyGEEMs",
     version="0.2.0",
     packages=find_packages(exclude=[".*tests.*"]),
     test_suite="tests",
@@ -15,15 +18,21 @@ setup(
     author_email="albert.kottke@gmail.com",
     description="Geotechnical earthquake engineering models implemented in Python.",
     license="MIT",
-    long_description=readme,
+    long_description=readme + "\n\n" + history,
     long_description_content_type="text/markdown",
-    url="http://github.com/arkottke/pyrvt",
+    install_requires=[
+        "numpy",
+        "numba",
+        "setuptools",
+        "scipy",
+    ],
+    url="http://github.com/arkottke/pygeems",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Environment :: Console",
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python",
         "Topic :: Scientific/Engineering",
         "Intended Audience :: Science/Research",
