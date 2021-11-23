ae5a7bd7b7c66436e434198b7436f930bcec1731
Albert Kottke <albert.kottke@gmail.com>
2021-11-23 14:20:48 -0800

    Fixed import order.

--- pygeems/__init__.py
+++ pygeems/__init__.py
@@ -3,13 +3,10 @@ import pathlib
 
 from pkg_resources import get_distribution
 
-FPATH_DATA = pathlib.Path(__file__).parent / "data"
-
 import scipy.constants
 
-from . import ground_motion
-from . import slope_disp
-from . import dyn_props
+FPATH_DATA = pathlib.Path(__file__).parent / "data"
+KPA_TO_ATM = scipy.constants.kilo / scipy.constants.atm
 
 __author__ = "Albert Kottke"
 __copyright__ = "Copyright 2018 Albert Kottke"
@@ -17,4 +14,6 @@ __license__ = "MIT"
 __title__ = "pygeems"
 __version__ = get_distribution("pygeems").version
 
-KPA_TO_ATM = scipy.constants.kilo / scipy.constants.atm
+from . import dyn_props
+from . import ground_motion
+from . import slope_disp
